d9d1bab1fafa419b9a4999faf6fe4d1f8f465c29
Mau <mauriciobermudez1404@gmail.com>
2025-11-06 11:31:39 -0600

    Implementacion clases Pasajero, Tiquete, Factura

--- IF2000_ProyectoFinal/src/main/java/domain/Factura.java
+++ IF2000_ProyectoFinal/src/main/java/domain/Factura.java
@@ -9,5 +9,75 @@ package main.java.domain;
  * @author mauri
  */
 public class Factura {
+    private String numeroFactura;
+    private Tiquete tiquete;
+    private double subtotal;
+    private double impuesto = 0.13;
+    private double total;
     
+    public Factura(String numeroFactura, Tiquete tiquete, double impuesto) {
+        this.numeroFactura = numeroFactura;
+        this.tiquete = tiquete;
+        this.impuesto = impuesto;
+        this.subtotal = tiquete.getPrecio();
+        this.total = calcularTotal();
+    }
+    
+    public double calcularTotal() {
+        return subtotal + (subtotal * impuesto);
+    }
+
+    public String getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public Tiquete getTiquete() {
+        return tiquete;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public double getImpuesto() {
+        return impuesto;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setNumeroFactura(String numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public void setTiquete(Tiquete tiquete) {
+        this.tiquete = tiquete;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public void setImpuesto(double impuesto) {
+        this.impuesto = impuesto;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Factura\nFactura N°: " + numeroFactura +
+               "\nPasajero: " + tiquete.getPasajero().getNombre();
+               "\nVuelo: " + tiquete.getVuelo().getOrigen() + " → " + tiquete.getVuelo().getDestino() +
+               "\nClase: " + tiquete.getClase() +
+               "\nSubtotal: ₡" + subtotal +
+               "\nIVA (13%): ₡" + (subtotal * impuesto) +
+               "\nTotal: ₡" + total;
+    }
+    
+    
+
 }
